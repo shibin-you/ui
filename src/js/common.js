c033d4dbbaa64afb4d54b5bbd79b933e60ed8fed
shibin-you <709594315@qq.com>
2019-03-27 21:24:27 +0800

    progressBar

---
+++ src/js/common.js
@@ -0,0 +1,27 @@
+var elementStyle = document.createElement('div').style
+
+var vendor = (() => {
+  var transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+  for (var key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key
+    }
+  }
+  return false
+})()
+
+function prefixStyle(style) {
+  if (vendor === false) {
+    return false
+  }
+  if (vendor === 'standard') {
+    return style
+  }
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
--- src/js/progress-bar.js
+++ src/js/progress-bar.js
@@ -1,13 +1,13 @@
-function ProgressBar(el) {
+function ProgressBar(el, cb) {
   var vm = this
   vm.el = el
   vm.progressBtn = el.getElementsByClassName('progress-btn')[0]
   vm.progressEl = el.getElementsByClassName('progress')[0]
   vm.initialed = false
   vm.touch = {}
-
   el.onclick = function(e) {
-    vm.setProgress(e.offsetX)
+    vm.setProgressByLen(e.offsetX)
+    vm._cb()
   }
   el.ontouchstart = function(e) {
     vm.initialed = true
@@ -19,14 +19,20 @@ function ProgressBar(el) {
     if (!vm.initialed) {
       return
     }
+    vm.touch.move = true
     const deltaX = e.touches[0].pageX - vm.touch.startX
     const offsetWidth = Math.min(Math.max(0, vm.touch.left + deltaX), vm.el.clientWidth - vm.progressBtn.clientWidth)
-    vm.setProgress(offsetWidth)
+    vm.setProgressByLen(offsetWidth)
   }
   el.ontouchend = function(e) {
+    if (vm.touch.move) {
+      vm._cb()
+    }
+
     vm.initialed = false
+    vm.touch.move = false
   }
-  vm.setProgress = function(x) {
+  vm.setProgressByLen = function(x) {
     if (x < 0) {
       return
     }
@@ -35,30 +41,13 @@ function ProgressBar(el) {
     var barWidth = vm.el.clientWidth - vm.progressBtn.clientWidth
     vm.percent = vm.progressEl.clientWidth / barWidth
   }
-}
-var elementStyle = document.createElement('div').style
-var vendor = (() => {
-  var transformNames = {
-    webkit: 'webkitTransform',
-    Moz: 'MozTransform',
-    O: 'OTransform',
-    ms: 'msTransform',
-    standard: 'transform'
+  vm.setProgress = function(percent) {
+    var len = percent * vm.el.clientWidth
+    vm.setProgressByLen(len)
   }
-  for (var key in transformNames) {
-    if (elementStyle[transformNames[key]] !== undefined) {
-      return key
+  vm._cb = function() {
+    if (cb && typeof cb === 'function') {
+      cb.call(vm, vm.percent)
     }
   }
-  return false
-})()
-
-function prefixStyle(style) {
-  if (vendor === false) {
-    return false
-  }
-  if (vendor === 'standard') {
-    return style
-  }
-  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
 }
